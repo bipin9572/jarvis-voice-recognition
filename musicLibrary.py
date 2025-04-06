b1151909c3043f650718569ed8a374f6b8f4b5d3
BIPIN SONY <132199755+bipin9572@users.noreply.github.com>
2025-04-06 23:17:46 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "bipin": "https://www.youtube.com/watch?v=U47Tr9BB_wE",
+    "march": "https://www.youtube.com/watch?v=tAhLHQv_VzU",
+    "skyfall": "https://www.youtube.com/watch?v=sZrTJesvJeo",
+    "wolf": "https://www.youtube.com/watch?v=KTmatjyd4KM" 
+
+}
